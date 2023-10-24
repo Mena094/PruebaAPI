8fec67ff4411263495778d071a4c348914a7f975
Mena0409 <walthermena0799@gmail.com>
2023-10-24 17:48:05 -0600

    Login Usuario

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express"//importando la libreria express.
 import dotenv from "dotenv"//importando la libreria dotenv.
-import personaRoute from "./routes/Personas.routes.js"
+import usuarioRoute from "./routes/Usuario.routes.js"
 const app  = express(); //ejecutando la libreria.
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.use("/Personas", personaRoute)//
+app.use("/api", usuarioRoute)//
 
 app.listen(PORT, ()=> {
     console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
